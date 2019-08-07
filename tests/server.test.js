e1b96f8b4b950a4a2e201b6065f80705f2b9d7f0
Jack Bridger <jacksbridger@gmail.com>
2019-08-07 16:34:41 +0100

    Basic supertest is working for server test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,9 +1,10 @@
 const test = require('tape');
 const supertest = require('supertest');
-
+const router = require('../src/router');
 
 test('testing supertest', (t) => {
     supertest(router)
+    .get('/')
     .end((err, res) => {
         t.equal(1,1);
         t.end();
