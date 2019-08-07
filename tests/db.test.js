7d6747861048127968a9db9d962283304c9e81e7
Jack Bridger <jacksbridger@gmail.com>
2019-08-07 17:29:03 +0100

    db tests are working, still need to write useful tests

--- tests/db.test.js
+++ tests/db.test.js
@@ -2,10 +2,16 @@ const test = require('tape');
 const runDbBuild = require('../src/database/db_build');
 const queries = require('../src/queries');
 
-test('tape is working in db test', (t) => {
+test('select all data from users table', (t) => {
     runDbBuild((err,res) => {
-        
+        t.error(err, 'No error');
+
+        let expected = [ { id: 1, name: 'Jon', gold_pieces: 20 }, { id: 2, name: 'Aria', gold_pieces: 20 } ];
+
+        queries.getData((err, result) => {
+            if (err) console.log(err);
+            t.deepEqual(result,expected, 'returns expected user data');
+            t.end();
+        })
     })
-    
-    t.end();
 })
