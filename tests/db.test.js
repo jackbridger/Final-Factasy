a21489c869fe3f5b89c3193aba3101158ec6f3f8
Victor Masson <victormasson21@gmail.com>
2019-08-08 11:25:29 +0100

    Create getItemsOwnedBy function

--- tests/db.test.js
+++ tests/db.test.js
@@ -6,7 +6,8 @@ test('select all data from users table', (t) => {
     runDbBuild((err,res) => {
         t.error(err, 'No error');
 
-        let expected = [ { id: 1, name: 'Jon', gold_pieces: 20 }, { id: 2, name: 'Aria', gold_pieces: 20 } ];
+        let expected = [ { id: 1, name: 'Jon', gold_pieces: 20 }, { id: 2, name: 'Aria', gold_pieces: 20 }, { id: 3, name: 'Hodor', gold_pieces: 20 },
+{ id: 4, name: 'Kevin', gold_pieces: 20 } ];
 
         queries.getData((err, result) => {
             if (err) console.log(err);
@@ -19,7 +20,7 @@ test('select all data from users table', (t) => {
 test('get the list of items that a user owns', (t) => {
     runDbBuild((err, res) => {
         t.error(err, 'No error');
-        let expected = [{item_name:'Toothbrush', item_description:'79% of medieval dentists believe dental hygiene can keep you alive.', item_power:10},{item_name:'Toothbrush', item_description:'79% of medieval dentists believe dental hygiene can keep you alive.', item_power:10}];
+        let expected = [{item_name:'Toothbrush', item_description:'79% of medieval dentists believe dental hygiene can keep you alive.', item_power:20},{item_name:'Toothbrush', item_description:'79% of medieval dentists believe dental hygiene can keep you alive.', item_power:20}];
 
         queries.getItemsOwnedBy(4, (err, result) => {
             if (err) console.log(err);
