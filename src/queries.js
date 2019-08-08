a5c91299b723c2d9ba3574cf6588cc632adfa26c
Tony Lomax <antl.lomax@gmail.com>
2019-08-08 11:00:13 +0100

    buyItem function Created first test to reduce gold in user table

--- src/queries.js
+++ src/queries.js
@@ -1,13 +1,24 @@
 const databaseConnection = require('./database/db_connection');
 const getData = (cb) => {
-    databaseConnection.query('SELECT * FROM users', (err, res) => {
+    databaseConnection.query('SELECT * FROM users ORDER BY id', (err, res) => {
         if (err) {
             cb(err);
         }
         else {
-            cb(null,res.rows);
+            cb(null, res.rows);
         }
     })
 }
 
-module.exports = { getData };
+const buyItem = cb => {
+    databaseConnection.query(`UPDATE users SET gold_pieces = gold_pieces -1 WHERE name = 'Jon';`, (err, res) => {
+        if (err) cb
+        else (
+            cb(null, res.rows)
+        )
+    })
+}
+
+
+module.exports = { getData, buyItem };
+
--- tests/db.test.js
+++ tests/db.test.js
@@ -3,15 +3,32 @@ const runDbBuild = require('../src/database/db_build');
 const queries = require('../src/queries');
 
 test('select all data from users table', (t) => {
-    runDbBuild((err,res) => {
+    runDbBuild((err, res) => {
         t.error(err, 'No error');
 
-        let expected = [ { id: 1, name: 'Jon', gold_pieces: 20 }, { id: 2, name: 'Aria', gold_pieces: 20 } ];
+        let expected = [{ id: 1, name: 'Jon', gold_pieces: 20 }, { id: 2, name: 'Aria', gold_pieces: 20 }];
 
         queries.getData((err, result) => {
             if (err) console.log(err);
-            t.deepEqual(result,expected, 'returns expected user data');
+            t.deepEqual(result, expected, 'returns expected user data');
             t.end();
+        })
+
+    })
+})
+
+test('Reduce gold in user table after purchase', (t) => {
+    runDbBuild((err, res) => {
+        t.error(err, 'No error');
+
+        let expected = [{ id: 1, name: 'Jon', gold_pieces: 19 }, { id: 2, name: 'Aria', gold_pieces: 20 }]
+        queries.buyItem((err, result) => {
+            if (err) console.log(err);
+        })
+        queries.getData((err, result) => {
+            if (err) console.log(err);
+            t.deepEqual(result, expected, 'Jon should have 1 less gold piece')
+            t.end()
         })
     })
 })
