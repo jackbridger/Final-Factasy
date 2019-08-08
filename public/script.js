fafee9f4fc96a59d6e5a5387f608d7203130e8ff
Victor Masson <victormasson21@gmail.com>
2019-08-08 17:41:04 +0100

    Buy now sends query + CSS fix

--- public/script.js
+++ public/script.js
@@ -18,20 +18,40 @@ const populateInventoryTable = (arr) => {
   const inventoryTable = document.querySelector(".inventory_table");
   arr.forEach(item => {
     let newItem = document.createElement('tr');
+    // Item details filled in
     let newItemName = document.createElement('th');
     newItemName.innerText = item.item_name;
     newItem.appendChild(newItemName);
-    let newItemPrice = document.createElement('th');
-    newItemPrice.innerText = item.item_price;
-    newItem.appendChild(newItemPrice);
     let newItemQuantity = document.createElement('th');
     newItemQuantity.innerText = item.item_quantity;
     newItem.appendChild(newItemQuantity);
+    let newItemPrice = document.createElement('th');
+    newItemPrice.innerText = item.item_price;
+    newItem.appendChild(newItemPrice);
+    // Button added
+    let newItemButton = document.createElement('button');
+    newItemButton.innerText = 'Buy now';
+    newItemButton.classList.add('buy-now');
+    newItemButton.setAttribute('onclick', `buyItem('${item.item_name}')`);
+    newItem.appendChild(newItemButton);
+    // Full new row added
     inventoryTable.appendChild(newItem);
   })
-
 }
 
+
+const buyItem = (itemName) => {
+  const xhr = new XMLHttpRequest();
+  const url = `/buyitem?${itemName}`
+  xhr.onreadystatechange = () =>{
+    if(xhr.readyState===4 && xhr.status===200){
+      const updatedTables=JSON.parse(xhr.responseText);
+      console.log(updatedTables);
+    }
+  }
+  xhr.open("GET", url);
+  xhr.send();
+}
 
 // const request = (url,cb) => {
 //   const xhr = new XMLHttpRequest();
